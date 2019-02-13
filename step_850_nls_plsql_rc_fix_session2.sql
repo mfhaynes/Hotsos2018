e845fe2780c96f7d55ed31a730d174f4312814b1
Michael Haynes <mf.haynes@gmail.com>
2019-02-13 17:39:56 +0000

    sync data format with db

--- step_850_nls_plsql_rc_fix_session2.sql
+++ step_850_nls_plsql_rc_fix_session2.sql
@@ -5,10 +5,10 @@ set echo on
 SELECT sysdate FROM dual;
 set echo off
 set autotrace on
-pause About to execute SELECT src_nls_test_func_w_input('DD-MON-YYYY') FROM dual for the first time...
+pause About to execute SELECT src_nls_test_func_w_input('DD-MON-YY') FROM dual for the first time...
 set echo on
-SELECT src_nls_test_func_w_input('DD-MON-YYYY') FROM dual;
+SELECT src_nls_test_func_w_input('DD-MON-YY') FROM dual;
 set echo off
-pause About to execute SELECT src_nls_test_func_w_input('DD-MON-YYYY') FROM dual for the second time...
+pause About to execute SELECT src_nls_test_func_w_input('DD-MON-YY') FROM dual for the second time...
 set echo on
-SELECT src_nls_test_func_w_input('DD-MON-YYYY') FROM dual;
+SELECT src_nls_test_func_w_input('DD-MON-YY') FROM dual;
