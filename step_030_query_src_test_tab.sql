4daa3bf8f3cb3bcdc78f63788aaca5e82dd5360f
Michael Haynes <mf.haynes@gmail.com>
2019-02-13 17:11:45 +0000

    add linesize

--- step_030_query_src_test_tab.sql
+++ step_030_query_src_test_tab.sql
@@ -1,7 +1,9 @@
+prompt login as rescache
+connect rescache@&database
 set echo off
 set autotrace on
 set timing on
-set lines 120
+set lines 150
 set pages 40
 pause About to execute SELECT AVG(FIELD2) FROM SRC_TEST_TAB...
 set echo on
