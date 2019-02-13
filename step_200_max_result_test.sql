cf709f561a1b0890d75dd6535b16711e9345e2d5
Michael Haynes <mf.haynes@gmail.com>
2019-02-13 17:26:16 +0000

    update query sizes

--- step_200_max_result_test.sql
+++ step_200_max_result_test.sql
@@ -6,19 +6,19 @@ set echo on
 show parameter result_cache_max
 set autotrace traceonly
 set echo off
-pause About to execute SELECT /*+ RESULT_CACHE */ * FROM SRC_TEST_TAB WHERE FIELD2 <= 800
+pause About to execute SELECT /*+ RESULT_CACHE */ * FROM SRC_TEST_TAB WHERE FIELD2 <= 400
 set echo on
-SELECT /*+ RESULT_CACHE */ * FROM SRC_TEST_TAB WHERE FIELD2 <= 800;
+SELECT /*+ RESULT_CACHE */ * FROM SRC_TEST_TAB WHERE FIELD2 <= 400;
 set echo off
-pause About to execute SELECT /*+ RESULT_CACHE */ * FROM SRC_TEST_TAB WHERE FIELD2 <= 800
+pause About to execute SELECT /*+ RESULT_CACHE */ * FROM SRC_TEST_TAB WHERE FIELD2 <= 400
 set echo on
-SELECT /*+ RESULT_CACHE */ * FROM SRC_TEST_TAB WHERE FIELD2 <= 800;
+SELECT /*+ RESULT_CACHE */ * FROM SRC_TEST_TAB WHERE FIELD2 <= 400;
 set echo off
-pause About to execute SELECT /*+ RESULT_CACHE */ * FROM SRC_TEST_TAB WHERE FIELD2 <= 1200
+pause About to execute SELECT /*+ RESULT_CACHE */ * FROM SRC_TEST_TAB WHERE FIELD2 <= 500
 set echo on
-SELECT /*+ RESULT_CACHE */ * FROM SRC_TEST_TAB WHERE FIELD2 <= 1200;
+SELECT /*+ RESULT_CACHE */ * FROM SRC_TEST_TAB WHERE FIELD2 <= 500;
 set echo off
-pause About to execute SELECT /*+ RESULT_CACHE */ * FROM SRC_TEST_TAB WHERE FIELD2 <= 1200
+pause About to execute SELECT /*+ RESULT_CACHE */ * FROM SRC_TEST_TAB WHERE FIELD2 <= 500
 set echo on
-SELECT /*+ RESULT_CACHE */ * FROM SRC_TEST_TAB WHERE FIELD2 <= 1200;
+SELECT /*+ RESULT_CACHE */ * FROM SRC_TEST_TAB WHERE FIELD2 <= 500;
 set autotrace off
