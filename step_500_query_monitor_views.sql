51b5e529b2c44e48a41c845328416423a4d09a1b
Michael Haynes <mf.haynes@gmail.com>
2019-02-20 17:19:55 +0000

    formatting changes

--- step_500_query_monitor_views.sql
+++ step_500_query_monitor_views.sql
@@ -1,8 +1,8 @@
 set echo off
 col name for a40
 col value for a20
-set lines 150
-set pages 50
+set lines 145
+set pages 40
 set autotrace off
 pause About to query V$RESULT_CACHE_STATISTICS...
 set echo on
@@ -23,11 +23,12 @@ set echo on
 SELECT * FROM v$result_cache_dependency;
 set echo off
 pause About to query V$RESULT_CACHE_DEPENDENCY with joins...
-col result_name for a75
-col dependency_name for a30
+col result_name for a65
+col dependency_name for a35
 col object_name for a30
 set echo on
 SELECT rcd.result_id, result.name AS result_name, dependency.name AS dependency_name, object.object_name
 FROM v$result_cache_dependency rcd JOIN v$result_cache_objects result ON rcd.result_id = result.id
 JOIN v$result_cache_objects dependency ON rcd.depend_id = dependency.id
-JOIN dba_objects object ON rcd.object_no = object.object_id;
+JOIN dba_objects object ON rcd.object_no = object.object_id
+order by rcd.result_id;
