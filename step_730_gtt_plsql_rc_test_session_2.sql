b02c3c8bf9a73f428c5c5517cdee23819d7aa798
Michael Haynes <mf.haynes@gmail.com>
2019-02-20 20:48:58 +0000

    adjust query

--- step_730_gtt_plsql_rc_test_session_2.sql
+++ step_730_gtt_plsql_rc_test_session_2.sql
@@ -2,9 +2,9 @@ set lines 145
 set pages 50
 set echo off
 set autotrace off
-pause About to execute SELECT * FROM src_gtt_test...
+pause About to execute SELECT * FROM src_gtt_test (first 500 rows)...
 set echo on
-SELECT * FROM src_gtt_test;
+SELECT * FROM src_gtt_test where rownum <=500;
 set echo off
 pause About to execute SELECT src_gtt_test_func() FROM dual...
 set autotrace on
